96627056e3d49d8203530d553a929279b008bcbf
raisultan <ki.xbozz@gmail.com>
2021-03-25 20:12:45 +0700

    python: nix build

---
+++ images/python/build.nix
@@ -0,0 +1,25 @@
+let
+  pkgs =
+    import ../common/nixpkgs.nix;
+
+  build_image =
+    import ../common/build_image.nix;
+
+  pythonPackages =
+    pkgs.python3.withPackages(ps: [
+      ps.cachetools
+      ps.nose
+      ps.numpy
+      ps.python-dateutil
+      ps.pytz
+      ps.six
+    ]);
+in
+build_image {
+  pkgs = pkgs;
+  name = "raisultan/python";
+  tag = "latest";
+  installedPackages = [
+    pythonPackages
+  ];
+}
