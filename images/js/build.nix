d6397f9721dff178fcafc59ac78ab3f0e1641c1f
raisultan <ki.xbozz@gmail.com>
2021-03-27 16:49:05 +0700

    js: build.nix adjusted

--- images/js/build.nix
+++ images/js/build.nix
@@ -4,6 +4,9 @@ let
 
   build_image =
     import ../common/build_image.nix;
+
+  imgNodeModules =
+    import ./img_node_modules/default.nix { pkgs = pkgs; };
 in
 build_image {
   pkgs = pkgs;
@@ -11,5 +14,9 @@ build_image {
   tag = "latest";
   installedPackages = [
     pkgs.nodejs
+  ];
+  env = [
+    "PATH=${pkgs.nodejs}/bin/:${imgNodeModules}/libexec/img_node_modules/node_modules/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+    "NODE_PATH=${imgNodeModules}/libexec/img_node_modules/node_modules"
   ];
 }
