73c7251ecd40a323dd376f519f026cde14fe134e
raisultan <ki.xbozz@gmail.com>
2021-04-09 03:11:58 +0700

    no offload request

--- loadbalancer/app/api/v1/endpoints/routes.py
+++ loadbalancer/app/api/v1/endpoints/routes.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter, status
 
+from app import schemas
+from app.api_driver import CodeRunnerAPIDriver
+
 router = APIRouter()
 
 
-@router.get('/', status_code=status.HTTP_200_OK)
-async def hello() -> dict[str, str]:
-    return {'detail': 'hello'}
+@router.post('/run', status_code=status.HTTP_200_OK)
+async def run(data: schemas.CodeRun) -> dict:
+    resp = await CodeRunnerAPIDriver.run(data)
+    return resp.json()
