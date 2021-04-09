4a29bf19aed4559533368e8e32f2b3bc5c3e1383
raisultan <ki.xbozz@gmail.com>
2021-04-10 00:02:11 +0700

    updated

--- loadbalancer/app/config/celery.py
+++ loadbalancer/app/config/celery.py
@@ -3,5 +3,5 @@ from celery import Celery
 from .settings import settings
 
 celery_app = Celery('celery_app')
-celery_app.config_from_object(settings, namespace='CELERY')
+celery_app.config_from_object(settings.Celery)
 celery_app.autodiscover_tasks(packages=['app.tasks'])
